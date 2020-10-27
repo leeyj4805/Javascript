641e33591c1fbda98d83538e7fd4ee9d7b570f53
leebingo <leeyj4805@gmail.com>
2020-10-28 01:41:18 +0900

    20.10.28 자바스크립트 정리

---
+++ js (ES5+)/operator.js
@@ -0,0 +1,48 @@
+// 1.String concatenation
+console.log('my' + 'cat');
+console.log('1' + 2);
+console.log(`string literals:
+...
+1 + 2 = ${1 + 2}`);
+
+
+console.log("ellie's \n\tbook");
+// \n 줄바꿈 \t tap키
+
+// 2. Numeric operators (연산)
+console.log(1 + 1); // add
+console.log(1 - 1); // substract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(5 % 2); // remainder
+console.log(2 ** 3); // exponentiation
+
+// 3. Increment and decrement operators
+let counter = 2;
+const preIncrement = ++ counter;
+// counter = counter + 1; (3)
+// preIncrement = counter; (3)
+console.log(`preIncrement: ${preIncrement}, counter: ${counter}`); // 3,3
+const postIncrement = counter++; (3)
+// postIncrement = counter; (3)
+// counter = counter + 1; (4)
+console.log(`preIncrement: ${preIncrement}, counter: ${counter}`); // 3,4
+const preDecrement = -- counter;
+console.log(`preDecrement: ${preDecrement}, counter: ${counter}`); // 3,3
+const postDecrement = counter--;
+console.log(`preDecrement: ${preDecrement}, counter: ${counter}`); // 3,2
+
+// 4. Assignment operators
+let x = 3;
+let y = 6;
+x += y; // x = x + y;
+x -= y;
+x *= y;
+x /= y;
+
+// 5. Comparison operators
+console.log(10 < 6); // less than
+console.log(10 <= 6); // less than or equal
+console.log(10 > 6); // greater than
+console.log(10 >= 6); // greater than or equal
+
--- js (ES5+)/variable.js
+++ js (ES5+)/variable.js
@@ -7,6 +7,7 @@
 // variable- 변수
 // 변경될 수 있는 값
 // let (added in ES6)
+// Mutable type - let
 
 // block scope
 let globalName = 'global name';
@@ -30,7 +31,100 @@ var age;
 }
 console.log(age);
 
-// Constants
+// 3. Constant r(read only)
+// 값을 할당하면 값이 변화가 되지 않는다.
+// - 보안상의 이유
+// - 동시에 값을 변경
+// Immutable-const
+
+const dayInWeek = 7;
+const maxNumber = 5;
+
+// note
+// immutable data types : prmitive type,frozen objects 
+// Mutable data types : object type
+// 4. Variable types
+// rw(read/write)
+// primitive type- 더이상 작은 단위로 나눠질 수 없는 한가지 아이템 (값 자체가 메모리에 저장)
+// number,string,boolean,null, undefiedn,symbol
+// object,- 싱글 아이템을 한 박스로 묶는것 (너무 커서 ref를 통해 메모리를 가리킴)
+// function 변수에 할당이 가능하다 (다른 변수에)
+
+const count = 17; // integer - 정수 (값은 num)
+const size = 17.1; // decimal number - 실수 (값은 num)
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+// number 
+
+const infinity = 1 / 0; // infinity 무한대의 값
+const negativeinfinity = -1 / 0; // -infinity - 무한대의 값
+const nAn = 'not a number' / 2; // 스트링을 출력하게 되면 NaN 출력
+console.log(infinity);
+console.log(negativeinfinity);
+console.log(nAn);
 
 
+// bigIng (ES6)
+
+const bigInt = 123215063101513213501350132103210531321030; // over (-2**53) ~2*53)
+console.log(`value: ${bigInt}, type:${typeof bigInt}`);
+Number.MAX_SAFE_INTEGER;
+
+// string
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!`; // template literals (string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+// ``백틱을 이용하면 스페이싱이나 문자열을 그대로 나오게 할 수 있다.
+
+//boolean
+// false : 0, null, undefined, NaN,''
+// true: and other value
+
+const canRead = true;
+const test = 3 < 1; // false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x = undefined;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+// symbol,동일한 스트링으로 만들어도 다른 심볼로 나타난다.
+// 고유한 식별자를 사용할때 사용
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+
+const gSsymbol1 = Symbol.for('id');
+const gSsymbol2 = Symbol.for('id');
+console.log(gSsymbol1 === gSsymbol2); // true
+console.log(`value: ${symbol1.decription}, type: ${typeof symbol1}`)
+// 심볼을 출력하고 싶을때는.decription을 사용한다.
+
+// object - 물건과 물체들을 대표할 수 있는 박스 
+const ellie = {name: 'ellie', age:20}; // ellie라는 오브젝트에 담음
+ellie.age = 21;
+
+// Dynamic typing : dynamically typed language
+// 에러가 발생되는 경우가 많다.
+// 그래서 타입스크립트가 나오게 되었다.
+
+let text = 'hello'; // string
+console.log(text.charAt(0)); // h출력
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1; // number
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5; // 스트링으로 변환되면서 합쳐짐
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '8' / '2'; // 숫자로 변환되면서 합쳐짐
+console.log(`value: ${text}, type: ${typeof text}`);
+//console.log(text.charAt(0)); // 에러가 발생됨 스트링으로 인식하기 때문에
 
